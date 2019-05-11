a027b5910a848fa16f96e989641ffecc52bdc99a
abramovainga75 <abramovainga75@gmail.com>
2019-05-11 19:04:30 +0300

    Добавьте файлы проекта.

---
+++ L3выч/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace L3выч
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Snow;
+            this.button1.Font = new System.Drawing.Font("Proxy 3", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.CadetBlue;
+            this.button1.Location = new System.Drawing.Point(1413, 547);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(380, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Решить систему и построить графики";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.zedGraphControl1.Location = new System.Drawing.Point(112, 70);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1021, 720);
+            this.zedGraphControl1.TabIndex = 48;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::L3выч.Properties.Resources.tyty;
+            this.panel1.Location = new System.Drawing.Point(1420, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(373, 253);
+            this.panel1.TabIndex = 75;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label1.Location = new System.Drawing.Point(1416, 381);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 22);
+            this.label1.TabIndex = 76;
+            this.label1.Text = "Y1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label2.Location = new System.Drawing.Point(1416, 492);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 22);
+            this.label2.TabIndex = 77;
+            this.label2.Text = "Правая граница";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1614, 379);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 22);
+            this.textBox1.TabIndex = 78;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1614, 490);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 22);
+            this.textBox2.TabIndex = 79;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1614, 445);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(175, 22);
+            this.textBox3.TabIndex = 81;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Snow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label3.Location = new System.Drawing.Point(1416, 446);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 22);
+            this.label3.TabIndex = 80;
+            this.label3.Text = "Y3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1614, 413);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(175, 22);
+            this.textBox4.TabIndex = 83;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Snow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label4.Location = new System.Drawing.Point(1416, 415);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 22);
+            this.label4.TabIndex = 82;
+            this.label4.Text = "Y2";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Snow;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label5.Location = new System.Drawing.Point(1413, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(376, 22);
+            this.label5.TabIndex = 84;
+            this.label5.Text = "Начальные условия";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::L3выч.Properties.Resources.look_com_ua_255862;
+            this.ClientSize = new System.Drawing.Size(1855, 932);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ L3выч/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using ZedGraph;
+
+namespace L3выч
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PaneSettings();
+        }
+        LineItem curve1;
+        GraphPane pane;
+        //Настраиваем график
+        private void PaneSettings()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.XAxis.Scale.MinAuto = true;
+            pane.XAxis.Scale.MaxAuto = true;
+            pane.YAxis.Scale.MinAuto = true;
+            pane.YAxis.Scale.MaxAuto = true;
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            pane.XAxis.Title.Text = "Ось X";
+            pane.YAxis.Title.Text = "Ось Y";
+            pane.Title.Text = "Графики функций";
+            pane.XAxis.MajorGrid.IsVisible = true;
+            pane.YAxis.MajorGrid.IsVisible = false;
+            pane.XAxis.MajorGrid.IsZeroLine = false;
+            pane.YAxis.MajorGrid.IsZeroLine = false;
+            pane.XAxis.Scale.FontSpec.Size = 12;
+            pane.YAxis.Scale.FontSpec.Size = 12;
+        }
+        //Исходна система уравнений для решения методом Эйлера
+        double dy1_dx(double y1, double y2, double y3)
+        {
+            return 2 * (y1) + y2 - 2 * (y3);
+        }
+        double dy2_dx(double y1, double y2, double y3)
+        {
+            return y1 - 2 * (y2);
+        }
+        double dy3_dx(double y1, double y2, double y3)
+        {
+            return y2 - y3;
+        }
+        //Собственные числа матрицы A
+        double a1 = 2.08613019765150, a2 = -2.51413692933529, a3 = -0.571993268316203;
+        //Матрицы М1, М2, М3
+        double[,] M2 = new double[3, 3] { { 0.0871324848481126,	-0.393327706979884,	0.115091885878961 }, { -0.169473197619615,	0.765025275326572,	-0.223854509360269 }, { 0.111927254680135,	-0.505254961660019,	0.147843058986419} };
+        double[,] M3 = new double[3, 3] { { -0.118392793539760,	0.304505125337512,	0.553227488161811 }, { -0.0829074542747664,	0.213237023559306,	0.387412579612278}, { -0.193706289806139,	0.498211415143651,	0.905154728509096} };
+        double[,] M1 = new double[3, 3] { { 1.03126030869165, 0.0888225816423723, -0.668319374040772 }, { 0.252380651894381, 0.0217377011141224, -0.163558070252009 }, { 0.0817790351260045, 0.00704354651636767, -0.0529977874955145 } };
+        //Решение методом Лагранжа-Сильвестра
+        double FUN1(double x, double y10, double y20, double y30)
+        {
+            return (M1[0, 0] * y10 + M1[0, 1] * y20 + M1[0, 2] * y30) * Math.Pow(Math.E, (a1 * x)) +
+                   (M2[0, 0] * y10 + M2[0, 1] * y20 + M2[0, 2] * y30) * Math.Pow(Math.E, (a2 * x)) +
+                   (M3[0, 0] * y10 + M3[0, 1] * y20 + M3[0, 2] * y30) * Math.Pow(Math.E, (a3 * x));
+        }
+        double FUN2(double x, double y10, double y20, double y30)
+        {
+            return (M1[1, 0] * y10 + M1[1, 1] * y20 + M1[1, 2] * y30) * Math.Pow(Math.E, (a1 * x)) +
+                   (M2[1, 0] * y10 + M2[1, 1] * y20 + M2[1, 2] * y30) * Math.Pow(Math.E, (a2 * x)) +
+                   (M3[1, 0] * y10 + M3[1, 1] * y20 + M3[1, 2] * y30) * Math.Pow(Math.E, (a3 * x));
+        }
+        double FUN3(double x, double y10, double y20, double y30)
+        {
+            return (M1[2, 0] * y10 + M1[2, 1] * y20 + M1[2, 2] * y30) * Math.Pow(Math.E, (a1 * x)) +
+                   (M2[2, 0] * y10 + M2[2, 1] * y20 + M2[2, 2] * y30) * Math.Pow(Math.E, (a2 * x)) +
+                   (M3[2, 0] * y10 + M3[2, 1] * y20 + M3[2, 2] * y30) * Math.Pow(Math.E, (a3 * x));
+        }
+        //Функция обработки события нажатия на кнопку "Решить систему и постоить график"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Проверка введенных значений на корректность
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+                    throw new Exception("Введите число.");
+                //Считываем и проверяем введенные данные
+               double left = 0;
+               double right = double.Parse(textBox2.Text);
+                if (left > right || left == right) throw new ApplicationException("Правая граница должна быть больше левой.");
+                if ((left > 300)||(right > 300)) { throw new Exception("Значение функции выходит за границу типа double."); }
+                //Списки точек для построения графиков
+                PointPairList F1 = new PointPairList();
+                PointPairList F2 = new PointPairList();
+                PointPairList F3 = new PointPairList();
+                //Начальные условия
+                double y10 = double.Parse(textBox1.Text);
+                double y20 = double.Parse(textBox4.Text);
+                double y30 = double.Parse(textBox3.Text);
+                if ((y10 == 0) && (y20 == 0)&&(y30==0)) { throw new Exception("Значения функций равны 0."); }
+                //Задаем шаг
+                double step = (right - left) / 200;
+                
+                //Метод Лагранжа-Сильвестра
+               
+                   //Очищаем списки точек
+                    F1.Clear();
+                    F2.Clear();
+                    F3.Clear();
+                    double y11 = 0.0, y21 = 0.0, y31 = 0.0;
+                    int i = 0;
+                    for (double x = left; x <= right; x += step)
+                    {
+                        //Вычисляем значения функций
+                        y11 = FUN1(x, y10, y20, y30);  
+                        y21 = FUN2(x, y10, y20, y30);
+                        y31 = FUN3(x, y10, y20, y30);
+                        //Добавляем точки в списки
+                        F1.Add(x, y11);
+                        F2.Add(x, y21);
+                        F3.Add(x, y31);
+                        i++;
+                    }
+                
+                //Настраиваем график
+                pane = zedGraphControl1.GraphPane;
+                pane.CurveList.Clear();
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+                //Настройка панели для построения графика
+                pane = zedGraphControl1.GraphPane;
+                zedGraphControl1.AutoSize = false; pane.XAxis.Scale.MaxAuto = true;
+                pane.XAxis.Scale.MinAuto = true;
+                pane.YAxis.Scale.MaxAuto = true;
+                pane.YAxis.Scale.MinAuto = true;
+                //Установка масштаба 
+                pane.XAxis.Scale.Min = left;
+                pane.XAxis.Scale.Max = right;
+                pane.XAxis.Scale.Format = "F2";
+                pane.XAxis.Scale.FontSpec.Size = 12;
+                pane.YAxis.Scale.FontSpec.Size = 12;
+                //Очищаем список кривых
+                pane.CurveList.Clear();
+                //Определяем заголовки
+                pane.Title.Text = "Графики функций";
+                pane.XAxis.Title.Text = pane.XAxis.Title.Text = "Ось X";
+                pane.YAxis.Title.Text = pane.YAxis.Title.Text = "Ось Y";
+                //Строим графики функций
+                Draw1(F1);
+                Draw2(F2);
+                Draw3(F3);
+            }
+            catch (Exception ea)
+            {
+                MessageBox.Show(ea.Message);
+            }
+        }
+        //Построение графика первой функции
+        private void Draw1(PointPairList list)
+        {
+            try
+            {
+                //Очищение панели
+                pane.CurveList.Clear();
+                //Построение графика функции
+                curve1 = pane.AddCurve("функция 1", list, Color.Gray, SymbolType.None);
+                //Задаем ширину линии
+                curve1.Line.Width = 2;
+                //Обновляем данные
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+            //Обработка исключений
+            catch (Exception m)
+            {
+                MessageBox.Show(m.Message);
+            }
+        }
+        //Построение графика второй функции
+        private void Draw2(PointPairList list)
+        {
+            //Сортировка массива точек
+            list.TrimExcess();
+            list.Sort();
+            try
+            {
+                //Построение графика функции
+                curve1 = pane.AddCurve("функция 2", list, Color.Blue, SymbolType.None);
+                //Задаем ширину линии
+                curve1.Line.Width = 2;
+                //Обновляем данные
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+            //Обработка исключений
+            catch (Exception m)
+            {
+                MessageBox.Show(m.Message);
+            }
+        }
+        //Построение графика второй функции
+        private void Draw3(PointPairList list)
+        {
+            //Сортировка массива точек
+            list.TrimExcess();
+            list.Sort();
+            try
+            {
+                //Построение графика функции
+                curve1 = pane.AddCurve("функция 3", list, Color.DarkCyan, SymbolType.None);
+                //Задаем ширину линии
+                curve1.Line.Width = 2;
+                //Обновляем данные
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+            }
+            //Обработка исключений
+            catch (Exception m)
+            {
+                MessageBox.Show(m.Message);
+            }
+        }
+        //Проверка
+        private void Ch(TextBox textBox1, KeyPressEventArgs e)
+        {
+            //Проверяем, не является ли запятая первым введенным символом
+            if (e.KeyChar == ',' && textBox1.Text.Length == 0)
+            //Выводим окно, сообщающее об ошибке ввода.
+            {
+                MessageBox.Show(this, "Введите число.", "Ошибка");
+                e.Handled = true;
+            }
+            //Проверяем, что минус является первым символом
+            if (e.KeyChar == '-' && textBox1.Text.Length != 0)
+            //Выводим окно, сообщающее об ошибке ввода.
+            {
+                MessageBox.Show(this, "Минус должен быть первым символом.", "Ошибка");
+                e.Handled = true;
+            }
+            //Проверяем, не ввел ли пользователь запятую второй раз.
+            if (e.KeyChar == ',' && textBox1.Text.IndexOf(",") != -1)
+            {
+                //Выводим окно, сообщающее об ошибке ввода.
+                { MessageBox.Show(this, "Запятая уже введена.", "Ошибка"); }
+                //Неправильно введенный символ не обрабатываем и не выводим его в TextBox.
+                e.Handled = true;
+            }
+            else
+            {
+                //Проверяем, не ввел ли пользователь минус второй раз.
+                if (e.KeyChar == ',' && textBox1.Text.IndexOf(",") != -1)
+                {
+                    //Выводим окно, сообщающее об ошибке ввода.
+                    { MessageBox.Show(this, "Минус уже введен.", "Ошибка"); }
+                    //Неправильно введенный символ не обрабатываем и не выводим его в TextBox.
+                    e.Handled = true;
+                }
+                //Проверяем введенный символ: если он не равен Backspase, запятой, минусу или цифре от 0 до 9.
+                if ((e.KeyChar != (char)45) && (e.KeyChar != (char)44) && (e.KeyChar != (char)8) && (e.KeyChar < (char)48 || e.KeyChar > (char)57))
+                {
+                    //Выводим окно, сообщающее об ошибке ввода.
+                    { MessageBox.Show(this, "Введен недопустимый символ. Введите число.", "Ошибка"); }
+                    //Неправильно введенный символ не обрабатываем и не выводим его в TextBox.
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Ch(textBox1, e);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Ch(textBox2, e);
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Ch(textBox4, e);
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Ch(textBox3, e);
+        }
+
+       
+    }
+
+}
+
